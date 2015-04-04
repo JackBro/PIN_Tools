acb35ffb4a827a0bb0fbe38b593fb2ea845d036a
hasherezade <hshrzd@gmail.com>
2015-04-04 16:46:12 +0200

    Update SectionsTracer.cpp

--- SectionsTracer.cpp
+++ SectionsTracer.cpp
@@ -1,7 +1,5 @@
 /*
- * SectionsTracer
- * Creative Commons by: hasherezade@op.pl
- *
+ * SectionsTracer, CC by: hasherezade@op.pl
  * Runs with: Intel PIN (https://software.intel.com/en-us/articles/pin-a-dynamic-binary-instrumentation-tool)
  *
  * Prints to <output_file> addresses of transitions from one sections to another
